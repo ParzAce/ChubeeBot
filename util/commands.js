acdc77c9accbbc9d1ce42757e88490e59ae7fd4a
mummmj6824 <72931524+mummmj6824@users.noreply.github.com>
2022-05-02 19:23:28 -0500

    Update commands.js

--- util/commands.js
+++ util/commands.js
@@ -48,37 +48,37 @@ const dojoTimes = (message) => {
     var cipankuDay = day
     var arburyDay = day
 
-    if (hours > 3) {
+    if (hours > 8) {
         denizDay = ("0" + (date_ob.getDate() + 1)).slice(-2)
     }
 
-    if (hours > 7) {
+    if (hours > 12) {
         omniDay = ("0" + (date_ob.getDate() + 1)).slice(-2)
     }
 
-    if (hours > 11) {
+    if (hours > 16) {
         tucmaDay = ("0" + (date_ob.getDate() + 1)).slice(-2)
     }
 
-    if (hours > 15) {
+    if (hours > 20) {
         kisiwaDay = ("0" + (date_ob.getDate() + 1)).slice(-2)
     }
 
-    if (hours > 19) {
+    if (hours > 24) {
         cipankuDay = ("0" + (date_ob.getDate() + 1)).slice(-2)
     }
 
-    if (hours > 23) {
+    if (hours > 4) {
         arburyDay = ("0" + (date_ob.getDate() + 1)).slice(-2)
     }
 
 
-    var denizDT = year + "-" + month + "-" + denizDay + " " + 3 + ":" + "00" + ":" + "00";
-    var omniDT = year + "-" + month + "-" + omniDay + " " + 7 + ":" + "00" + ":" + "00";
-    var tucmaDT = year + "-" + month + "-" + tucmaDay + " " + 11 + ":" + "00" + ":" + "00";
-    var kisiwaDT = year + "-" + month + "-" + kisiwaDay + " " + 15 + ":" + "00" + ":" + "00";
-    var cipankuDT = year + "-" + month + "-" + cipankuDay + " " + 19 + ":" + "00" + ":" + "00";
-    var arburyDT = year + "-" + month + "-" + arburyDay + " " + 23 + ":" + "00" + ":" + "00";
+    var denizDT = year + "-" + month + "-" + denizDay + " " + 8 + ":" + "00" + ":" + "00";
+    var omniDT = year + "-" + month + "-" + omniDay + " " + 12 + ":" + "00" + ":" + "00";
+    var tucmaDT = year + "-" + month + "-" + tucmaDay + " " + 16 + ":" + "00" + ":" + "00";
+    var kisiwaDT = year + "-" + month + "-" + kisiwaDay + " " + 20 + ":" + "00" + ":" + "00";
+    var cipankuDT = year + "-" + month + "-" + cipankuDay + " " + 24 + ":" + "00" + ":" + "00";
+    var arburyDT = year + "-" + month + "-" + arburyDay + " " + 4 + ":" + "00" + ":" + "00";
 
 
     //find unix for each island
