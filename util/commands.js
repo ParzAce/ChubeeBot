7e1ce71f997ba51a2408bb46f564f7b8062bf7dc
mummmj6824 <72931524+mummmj6824@users.noreply.github.com>
2022-05-30 20:07:47 -0500

    Update commands.js

--- util/commands.js
+++ util/commands.js
@@ -44,12 +44,12 @@ const leveling = (message, commandString) => {
     if (commandString[1] === "normal" && commandString[2] > 0 && commandString[2] < 9) {
         message.author.send("*Bzzzzt*\n\n\nWe have recieved your request! \n\nSomeone will be reaching out to you shortly <a:lumaChubeePat:964325660803858452>\n\n\n*Bzzzzt*")
         inboundChannel.send({
-            content: user + " has requested a normal leveling package for " + commandString[2] + " Tems\n\n\n Please react with a checkmark if you want to take the request"
+            content: "---------------------------------------------------------------\n" + user + " has requested a normal leveling package for " + commandString[2] + " Tems\n\n\n Please react with a checkmark if you want to take the request"
             });
     } else if (commandString[1] === "rush" && commandString[2] > 0 && commandString[2] < 5) {
         message.author.send("*Bzzzzt*\n\n\nWe have recieved your request! \n\nSomeone will be reaching out to you shortly <a:lumaChubeePat:964325660803858452>\n\n\n*Bzzzzt*")
         inboundChannel.send({
-            content: user + " has requested a rush leveling package for " + commandString[2] + " Tems\n\n\n Please react with a checkmark if you want to take the request"
+            content: "---------------------------------------------------------------\n" + user + " has requested a rush leveling package for " + commandString[2] + " Tems\n\n\n Please react with a checkmark if you want to take the request"
             });
     } else {
         message.author.send("*Bzzzzt*\n\n\nYou might have entered the command in wrong<a:lumaChubeePat:964325660803858452>\n\n\n*Bzzzzt*")
@@ -64,18 +64,18 @@ const rental = (message, commandString) => {
     if (commandString[1] === "radar") {
         message.author.send("*Bzzzzt*\n\n\nWe have recieved your request! \n\nSomeone will be reaching out to you shortly <a:lumaChubeePat:964325660803858452>\n\n\n*Bzzzzt*")
         inboundChannel.send({
-            content: user + " has requested a radar package\n\n\n Please react with a checkmark if you want to take the request"
+            content: "---------------------------------------------------------------\n" + user + " has requested a radar package\n\n\n Please react with a checkmark if you want to take the request"
             });
     } else if (commandString[1] === "full") {
         console.log("message")
         message.author.send("*Bzzzzt*\n\n\nWe have recieved your request! \n\nSomeone will be reaching out to you shortly <a:lumaChubeePat:964325660803858452>\n\n\n*Bzzzzt*")
         inboundChannel.send({
-            content: user + " has requested a full package\n\n\n Please react with a checkmark if you want to take the request"
+            content: "---------------------------------------------------------------\n" + user + " has requested a full package\n\n\n Please react with a checkmark if you want to take the request"
             });
     } else if (commandString[1] === "plain") {
         message.author.send("*Bzzzzt*\n\n\nWe have recieved your request! \n\nSomeone will be reaching out to you shortly <a:lumaChubeePat:964325660803858452>\n\n\n*Bzzzzt*")
         inboundChannel.send({
-            content: user + " has requested a plain package\n\n\n Please react with a checkmark once the rental team has been rented out"
+            content: "---------------------------------------------------------------\n" + user + " has requested a plain package\n\n\n Please react with a checkmark once the rental team has been rented out"
             });
     } else {
         message.author.send("*Bzzzzt*\n\n\nYou might have entered the command in wrong<a:lumaChubeePat:964325660803858452>\n\n\n*Bzzzzt*")
